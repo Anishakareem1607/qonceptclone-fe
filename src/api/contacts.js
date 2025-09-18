c93a84699b101d587e919acaa0c496b0d29c0edf
anisha <anishatk012@gmail.com>
2025-09-18 19:00:12 +0530

    message

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-
-const API_BASE =
-  import.meta.env.VITE_API_URL || "http://localhost:5000"; 
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export async function fetchContacts() {
   const res = await axios.get(`${API_BASE}/api/contacts`);
