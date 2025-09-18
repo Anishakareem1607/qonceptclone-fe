29b8cecf66c447ec31c2201f7b2a6513c02505a7
anisha <anishatk012@gmail.com>
2025-09-18 18:34:04 +0530

    message

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://qonceptclone-backend.onrender.com",
-     
+      "/api": "http://localhost:5000", 
     },
   },
 });
